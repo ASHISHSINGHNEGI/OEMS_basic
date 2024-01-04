6be201ea75eb88e331636930a858ce509a1ab526
ashishsinghnegi <ashishsinghnegi381@gmail.com>
2024-01-04 13:55:36 +0530

    student page ready firebase enabled

--- home.js
+++ home.js
@@ -1,18 +1,18 @@
-import { initializeApp } from "firebase/app";
-import { getFirestore, collection, addDoc } from "firebase/firestore";
-
-const form = document.getElementById("openElectiveSelectionForm");
-const submitButton = document.getElementById("submitButton");
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+import {
+  getFirestore,
+  collection,
+  addDoc,
+} from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyC-DOC2Tz7Po9lbzIumZ14-KKoBQjgp1_U",
-  authDomain: "openelective2-95186.firebaseapp.com",
-  databaseURL:
-    "https://openelective2-95186-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "openelective2-95186",
-  storageBucket: "openelective2-95186.appspot.com",
-  messagingSenderId: "354014652523",
-  appId: "1:354014652523:web:429930cd8b600ef8fba92e",
+  apiKey: "AIzaSyB9fWu3R5pfKC3BWBkPjc9zRQB7lWpu7ow",
+  authDomain: "oemsbasic.firebaseapp.com",
+  projectId: "oemsbasic",
+  storageBucket: "oemsbasic.appspot.com",
+  messagingSenderId: "407214565237",
+  appId: "1:407214565237:web:2eb8f7910bce4b00abd53c",
 };
 
 // Initialize Firebase
@@ -21,13 +21,8 @@ const app = initializeApp(firebaseConfig);
 // Initialize Cloud Firestore and get a reference to the service
 const db = getFirestore(app);
 
-document.addEventListener("DOMContentLoaded", function () {
-  // Get the form and submit button elements
-
-  // Function to handle form submission
-  function handleSubmit(event) {
-    event.preventDefault(); // Prevents the default form submission behavior
-    // Retrieve form input values
+async function store() {
+  try {
     const name = document.getElementById("name").value;
     const email = document.getElementById("email").value;
     const instituteId = document.getElementById("InstituteId").value;
@@ -35,10 +30,11 @@ document.addEventListener("DOMContentLoaded", function () {
     const branch = document.getElementById("Branch").value;
     const subject1 = document.getElementById("subject1").value;
     const subject2 = document.getElementById("subject2").value;
-
-    // Add data to Firestore collection
-    db.collection("users")
-      .add({
+    const message = document.getElementById("message");
+    const form = document.getElementById("openElectiveSelectionForm");
+    const docRef = await addDoc(
+      collection(db, "studentSubjectSelectionDetails"),
+      {
         name: name,
         email: email,
         instituteId: instituteId,
@@ -46,17 +42,15 @@ document.addEventListener("DOMContentLoaded", function () {
         branch: branch,
         subject1: subject1,
         subject2: subject2,
-        timestamp: firebase.firestore.FieldValue.serverTimestamp(),
-      })
-      .then((docRef) => {
-        console.log("Document written with ID: ", docRef.id);
-        // Reset form after successful submission
-        form.reset();
-      })
-      .catch((error) => {
-        console.error("Error adding document: ", error);
-      });
+      }
+    );
+    message.innerText = `Document saved -  ${name}`;
+    console.log("Document written with ID: ", docRef.id);
+    form.reset();
+  } catch (e) {
+    console.error("Error adding document: ", e);
   }
+}
+const button = document.getElementById("submitButton");
 
-  form.addEventListener("submit", handleSubmit);
-});
+button.addEventListener("click", store);
